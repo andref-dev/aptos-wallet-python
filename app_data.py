9dba3247d7d64fb6d7e22be6e8392faa9529acbf
andref-dev <andre.hl2@outlook.com>
2022-11-09 18:34:40 -0300

    - Updated clients url comments

--- app_data.py
+++ app_data.py
@@ -3,12 +3,15 @@ from aptos_sdk.client import RestClient, FaucetClient
 
 class AppData:
     def __init__(self):
+        # To run the application pointing to Aptos DevNet, use these urls
+        #rest_url = "https://fullnode.devnet.aptoslabs.com/v1"
+        #faucet_url = "https://tap.devnet.prod.gcp.aptosdev.com"
 
-        rest_url = "https://fullnode.devnet.aptoslabs.com/v1"
-        faucet_url = "https://tap.devnet.prod.gcp.aptosdev.com"
-
-        #rest_url = "http://0.0.0.0:8080/v1"
-        #faucet_url = "http://0.0.0.0:8000"
+        # To run the application pointing to a local version of Aptos, use these urls
+        # You can learn how to run a local version of aptos here:
+        # https://aptos.dev/nodes/local-testnet/run-a-local-testnet/
+        rest_url = "http://0.0.0.0:8080/v1"
+        faucet_url = "http://0.0.0.0:8000"
 
         self.users = {}
         self.nft_collections = {}
